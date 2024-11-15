5d46fcce39de2bcc01624acaee25799971a676b5
erhan3861 <71086723+erhan3861@users.noreply.github.com>
2024-11-15 20:15:00 +0300

    Add files via upload

---
+++ 7_kedi_yuzu_tespiti.py
@@ -0,0 +1,50 @@
+import cv2
+import os
+
+files = os.listdir()
+print(files)
+
+img_path = []
+
+for f in files:
+    if f.startswith("cat"):
+        img_path.append(f)
+
+print(img_path)
+
+cf = cv2.CascadeClassifier("haarcascade_frontalcatface_extended.xml")
+
+for i in img_path:
+    img = cv2.imread(i)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    rect = cf.detectMultiScale(gray_img, scaleFactor = 1.03, minNeighbors=2)
+
+    for (j, (x, y, w, h)) in enumerate(rect):
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
+
+    cv2.imshow(i, img)
+
+    if cv2.waitKey(0) & 0xFF == ord('g'): continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
